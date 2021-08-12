4dd3454a99da5cfcea234cc4163c96071fc7f158
zvercodebender <rbroker@digital.ai>
2021-08-12 16:57:33 -0400

    Change message

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 #test123 pipeline
 @app.route("/")
 def hello():
-    return "Hello, this is K8 deployment demo using DAI Release and Deploy server"
+    return "Hello Rick, this is K8 deployment demo using DAI Release and Deploy server"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
